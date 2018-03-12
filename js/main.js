530b295329560e0cb3f60a0e5ca941ccd3e06f8e
Meena <meena_jim@yahoo.com>
2018-03-13 09:59:39 +1100

    TIC TAC TOE VALIDATION

--- js/main.js
+++ js/main.js
@@ -11,33 +11,87 @@ $(document).ready(function(){
       ['','','']
     ];
 
-// const switchPlayer= function(token){
-// console.log(token);
-//   if (token === player1)
-//   {
-//     token = player2;
-//     console.log(token);
-//     return token;
-//   }
-// else if(token === player2)
-//   {
-//     token = player1;
-//   }
-//   return token;
-// }
 
+
+const resetBoard = function(){
+  //Clear the gameboard array
+  for(let i=0;i<3;i++)
+  {
+    for(let j=0;j<3;j++)
+    {
+      gameBoard[i][j]="";
+      $('.column[data-rowid='+i+'][data-colid='+j+']').html('');
+
+    };
+    //$('.column[data-rowid=0][data-colid=0]').html('');
+
+  };
+
+};
+
+const checkWinner = function(){
+  // console.log(gameBoard);
+  //check horizontal
+  for(let i = 0; i < 3; i++)
+  {
+    if(gameBoard[i][0]!= "" && gameBoard[i][0] === gameBoard[i][1] && gameBoard[i][0] === gameBoard[i][2])
+    {
+      console.log(`${gameBoard[i][0]} is the winner-horizontal`);
+      return gameBoard[i][0];
+    }
+  };
+  //check vertical
+  for(let j=0;j<3;j++)
+  {
+    if(gameBoard[0][j]!= "" && gameBoard[0][j] === gameBoard[1][j] && gameBoard[0][j] === gameBoard[2][j])
+    {
+      console.log(`${gameBoard[0][j]} is the winner - vertical`);
+      return gameBoard[0][j];
+    }
+  }
+  //check diagonal 1
+  if(gameBoard[0][0]!="" && gameBoard[0][0] === gameBoard[1][1] && gameBoard[0][0] === gameBoard[2][2])
+  {
+    console.log(`${gameBoard[0][0]} is the winner- diagonal`);
+    return gameBoard[0][0];
+  }
+  //check diagonal 2
+  if(gameBoard[0][2]!="" && gameBoard[0][2] === gameBoard[1][1] && gameBoard[0][2]===gameBoard[2][0])
+  {
+    console.log(`${gameBoard[0][2]} is the winner -diagonal`);
+    return gameBoard[0][2];
+  }
+  //check draw
+  let counter=0;
+    for(let i = 0;i < 3; i++)
+    {
+      for(let j = 0;j<3;j++)
+      {
+        if(gameBoard[i][j]!= "")
+        {
+          counter= counter + 1;
+        }
+      }
+    }
+    //if the entire board is filled and there is no winner then its a draw
+    if(counter===9)
+    {
+      return "draw" ;
+    }
+  }
+//when a column is clicked
     $('.column').click(function()
     {
-    //console.log(`token is ${token}`)
+    //if there is an X or O in the selected column, then return
     if ($(this).html()==="X" || $(this).html()==="O") {
      return;
     }
-
-
+     //setting player1(X) to the selected column
      $(this).html(token);
 
     const current_row = $(this).data('rowid');
     const current_col = $(this).data('colid');
+
     gameBoard[current_row][current_col] = token;
     //prints the selected row and column
     //console.log(current_row, current_col);
@@ -47,7 +101,6 @@ $(document).ready(function(){
     //console.log(`value of the gameBoard is ${gameBoard[current_row][current_col]}`);
     //print the full gameboard
     //console.log(gameBoard);
-    // switchPlayer
 
     if (token === player1)
     {
@@ -61,78 +114,33 @@ $(document).ready(function(){
 
 //    });
 
-const checkWinner = function(){
-  // console.log(gameBoard);
 
-  //check horizontal
-  for(let i = 0; i < 3; i++)
-  {
-    if(gameBoard[i][0]!= "" && gameBoard[i][0] === gameBoard[i][1] && gameBoard[i][0] === gameBoard[i][2])
+let winner = checkWinner();
+if(winner)
     {
-      console.log(`${gameBoard[i][0]} is the winner-horizontal`);
-      return gameBoard[i][0];
-    }
-  };
-
-
-  //check vertical
-  for(let j=0;j<3;j++)
-  {
-    if(gameBoard[0][j]!="" && gameBoard[0][j] === gameBoard[1][j] && gameBoard[0][j] === gameBoard[2][j])
+    $('.winner').css("color","aqua");
+    if(winner ==='X')
     {
-      console.log(`${gameBoard[0][j]} is the winner - vertical`);
-      return gameBoard[0][j];
+    $('.winner').html(`Player 1 using token:${winner} WINS!!!`);
     }
-
-  }
-  //check diagonal
-  if(gameBoard[0][0]!="" && gameBoard[0][0] === gameBoard[1][1] && gameBoard[0][0] === gameBoard[2][2])
-  {
-
-    console.log(`${gameBoard[0][0]} is the winner- diagonal`);
-    return gameBoard[0][0];
-  }
-
-
-  //check diagonal
-  if(gameBoard[0][2]!="" && gameBoard[0][2] === gameBoard[1][1] && gameBoard[0][2]===gameBoard[2][0])
-  {
-
-    console.log(`${gameBoard[0][2]} is the winner -diagonal`);
-    return gameBoard[0][2];
-  }
-
-
-
-//check if there are any empty cells
-  // for(let i = 0;i < 3; i++)
-  // {
-  //   for(let j = 0;j<3;j++)
-  //   {
-  //     if(gameBoard[i][j]= "")
-  //     return false;
-  //   }
-  // }
-}
-checkWinner();
-//if there is a winner, display the output
-// console.log(`${checkWinner()} is the winner`);
-// console.log(`${checkWinner()} is the winner`);
-
-//Clear the gameBoard
-$('#reset').click(function(){
-
-//Clear the gameboard array
-for(let i=0;i<3;i++)
-{
-  for(let j=0;j<3;j++)
-  {
-    gameBoard[i][j]="";
-  };
-};
-console.log(gameBoard);
+    if (winner === 'O')
+    {
+    $('.winner').html(`Player 2 using token:${winner} WINS!!!`);
+    }
+    if (winner === 'draw')
+    {
+    $('.winner').html(`Game is a draw!!`);
+    }
+    //resetting to stop playing
+    resetBoard();
+    //  console.log(`${checkWinner()} is  the winner`);
+    }
 });
 
-});
-
+    //Clear the gameBoard
+    $('#reset').click(function(){
+    resetBoard();
+    $('.winner').html(' ');
+    //console.log(gameBoard);
   });
+});
