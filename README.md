b80cd2b366a388090904fd0db1c374941b918a0a
Meena <meena_jim@yahoo.com>
2018-03-14 10:11:52 +1100

    MORE VALIDATION

---
+++ README.md
@@ -0,0 +1,50 @@
+# Project 0
+
+Some regular text.
+
+
+# subheadings
+### subheadings
+some **regular** text.
+some _more_ regular _text_.
+This is a ***new*** paragraph.
+## Lists
+* list item
+* second item
+  * sub item
+  * sub item two
+
+### Marx Brothers
+1. Groucho
+1. Groucho
+1. Groucho
+
+![Bill Murray](http://fillmurray.com/50/70)
+
+http://twitter.com
+
+[Twitter or
+whatever](http://twitter.com/)
+
+To quote someone famous:
+>Famous inspirational quote
+
+I dont find it very inspirational
+
+You should use the `<section>` tag.
+
+There are two many variables called `score` in this project.
+
+
+## Github Flavoured MarkDown
+
+### Function expression example
+```javascript
+const sayHello = function(){
+console.log('Hello');
+}
+```
+
+@coldhead @aparkinbotswana
+
+~~this is struck out~~ updated docs
--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ console.log("Tic Tac Toe");
 
 const player1 = 'X';
 const player2 = 'O';
+const noplayer = '';
 let token = player1;
 
 $(document).ready(function(){
@@ -21,7 +22,7 @@ const resetBoard = function(){
     {
       gameBoard[i][j]="";
       $('.column[data-rowid='+i+'][data-colid='+j+']').html('');
-
+      token=player1;
     };
     //$('.column[data-rowid=0][data-colid=0]').html('');
 
@@ -34,31 +35,35 @@ const checkWinner = function(){
   //check horizontal
   for(let i = 0; i < 3; i++)
   {
-    if(gameBoard[i][0]!= "" && gameBoard[i][0] === gameBoard[i][1] && gameBoard[i][0] === gameBoard[i][2])
+    if(gameBoard[i][0]!== "" && gameBoard[i][0] === gameBoard[i][1] && gameBoard[i][0] === gameBoard[i][2])
     {
       console.log(`${gameBoard[i][0]} is the winner-horizontal`);
+      token=noplayer;
       return gameBoard[i][0];
     }
   };
   //check vertical
   for(let j=0;j<3;j++)
   {
-    if(gameBoard[0][j]!= "" && gameBoard[0][j] === gameBoard[1][j] && gameBoard[0][j] === gameBoard[2][j])
+    if(gameBoard[0][j]!== "" && gameBoard[0][j] === gameBoard[1][j] && gameBoard[0][j] === gameBoard[2][j])
     {
       console.log(`${gameBoard[0][j]} is the winner - vertical`);
+      token = noplayer;
       return gameBoard[0][j];
     }
   }
   //check diagonal 1
-  if(gameBoard[0][0]!="" && gameBoard[0][0] === gameBoard[1][1] && gameBoard[0][0] === gameBoard[2][2])
+  if(gameBoard[0][0]!== "" && gameBoard[0][0] === gameBoard[1][1] && gameBoard[0][0] === gameBoard[2][2])
   {
     console.log(`${gameBoard[0][0]} is the winner- diagonal`);
+    token = noplayer;
     return gameBoard[0][0];
   }
   //check diagonal 2
-  if(gameBoard[0][2]!="" && gameBoard[0][2] === gameBoard[1][1] && gameBoard[0][2]===gameBoard[2][0])
+  if(gameBoard[0][2]!== "" && gameBoard[0][2] === gameBoard[1][1] && gameBoard[0][2]===gameBoard[2][0])
   {
     console.log(`${gameBoard[0][2]} is the winner -diagonal`);
+    token = noplayer;
     return gameBoard[0][2];
   }
   //check draw
@@ -74,7 +79,7 @@ const checkWinner = function(){
       }
     }
     //if the entire board is filled and there is no winner then its a draw
-    if(counter===9)
+    if(counter === 9)
     {
       return "draw" ;
     }
@@ -82,17 +87,22 @@ const checkWinner = function(){
 //when a column is clicked
     $('.column').click(function()
     {
-    //if there is an X or O in the selected column, then return
-    if ($(this).html()==="X" || $(this).html()==="O") {
-     return;
-    }
+      //if there is an X or O in the selected column, then return
+      if ($(this).html() === "X" || $(this).html() === "O"){
+          return;
+      }
+
+
+
+    $(this).html(token);
+    // console.log($(this).html(token));
+
+   const current_row = $(this).data('rowid');
+   const current_col = $(this).data('colid');
+
+   gameBoard[current_row][current_col] = token;
      //setting player1(X) to the selected column
-     $(this).html(token);
 
-    const current_row = $(this).data('rowid');
-    const current_col = $(this).data('colid');
-
-    gameBoard[current_row][current_col] = token;
     //prints the selected row and column
     //console.log(current_row, current_col);
     //print that particular row in gameBoard
@@ -115,13 +125,19 @@ const checkWinner = function(){
 //    });
 
 
-let winner = checkWinner();
+//console.log('the winner is!!!! ' + winner);
+
+let winner=checkWinner();
 if(winner)
     {
-    $('.winner').css("color","aqua");
+   $('.winner').css("color","aqua");
+    // $('.winner').addClass("animated zoomIn");
     if(winner ==='X')
     {
+
+    $('.winner').html(`${winner} wins`);
     $('.winner').html(`Player 1 using token:${winner} WINS!!!`);
+
     }
     if (winner === 'O')
     {
@@ -131,10 +147,16 @@ if(winner)
     {
     $('.winner').html(`Game is a draw!!`);
     }
+  //  $('.winner').animate({'fontSize':"4em"});
+    //$('.winner').stop(true,true);
     //resetting to stop playing
-    resetBoard();
-    //  console.log(`${checkWinner()} is  the winner`);
+    // resetBoard();
+    // if (winner === "X" || winner === "O") {
+    //     return;
+    // }
+     // console.log(`${checkWinner()} is  the winner`);
     }
+
 });
 
     //Clear the gameBoard
